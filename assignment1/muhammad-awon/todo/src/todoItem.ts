2bf67ac101c7cb9702e2df8f92c59c4e326d1291
Muhammad Awon <awon195@gmail.com>
2021-07-13 16:16:06 +0500

    v3 remove keyword public from all methods (including constructor)

--- assignment1/muhammad-awon/todo/src/todoItem.ts
+++ assignment1/muhammad-awon/todo/src/todoItem.ts
@@ -1,10 +1,10 @@
 export class TodoItem {
     // use TS features that allow class to be defined concisely
-    public constructor(public id: number, public task: string, public complete: boolean=false) {
+    // TS by default assumes that all methods and properties are public, unless explicitly defined
+    constructor(public id: number, public task: string, public complete: boolean=false) {
         // no statements required
     }
 
-    // TS by default assumes that all methods and properties are public, unless explicitly defined
     printDetails(): void {
         console.log(`${this.id}\t${this.task}\t${this.complete
         ? "\t(complete)": ""}`);
