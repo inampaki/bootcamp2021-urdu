8ae6973ebe42b8dc48c8823c9fac2360678b76cb
Muhammad Awon <awon195@gmail.com>
2021-07-13 17:04:11 +0500

    write index.ts scripte to declare dependencies on TodoItem & TodoCollection

--- assignment1/muhammad-awon/todo/dist/index.js
+++ assignment1/muhammad-awon/todo/dist/index.js
@@ -1,2 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todoItem_1 = require("./todoItem");
+const todoCollection_1 = require("./todoCollection");
+// create a variable to store list of ids and tasks
+let thingsTodo = [
+    new todoItem_1.TodoItem(1, "Morning workout"), new todoItem_1.TodoItem(2, "Learn new thing"),
+    new todoItem_1.TodoItem(3, "Code time"), new todoItem_1.TodoItem(4, "Help someone", true)
+];
+// create a variable that add thingsToDo in TodoCollection class
+let collection = new todoCollection_1.TodoCollection("Awon", thingsTodo);
 console.clear();
-console.log("Awon's Todo List");
+console.log(`${collection.userName}'s Todo List`);
+// add new task
+let newId = collection.addTodo("Think of new project");
+let todoItem = collection.getTodoById(newId);
+// display todoItem
+todoItem.printDetails();
--- assignment1/muhammad-awon/todo/src/index.ts
+++ assignment1/muhammad-awon/todo/src/index.ts
@@ -1,2 +1,21 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+
+// create a variable to store list of ids and tasks
+let thingsTodo: TodoItem[] = [
+    new TodoItem(1, "Morning workout"), new TodoItem(2, "Learn new thing"),
+    new TodoItem(3, "Code time"), new TodoItem(4, "Help someone", true)
+];
+
+// create a variable that add thingsToDo in TodoCollection class
+let collection: TodoCollection = new TodoCollection("Awon", thingsTodo);
+
 console.clear();
-console.log("Awon's Todo List");
+console.log(`${collection.userName}'s Todo List`);
+
+// add new task
+let newId: number = collection.addTodo("Think of new project");
+let todoItem: TodoItem = collection.getTodoById(newId);
+
+// display todoItem
+todoItem.printDetails();
