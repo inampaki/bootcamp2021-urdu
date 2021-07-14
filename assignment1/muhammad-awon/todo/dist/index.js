50e3d390a0f2d6cc6054a63de00d317d421e2b65
Muhammad Awon <awon195@gmail.com>
2021-07-14 16:01:09 +0500

    first introduction of inquirer in the project

--- assignment1/muhammad-awon/todo/dist/index.js
+++ assignment1/muhammad-awon/todo/dist/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const todoItem_1 = require("./todoItem");
 const todoCollection_1 = require("./todoCollection");
+const inquirer = require("inquirer");
 // create a variable to store list of ids and tasks
 let thingsTodo = [
     new todoItem_1.TodoItem(1, "Morning workout"), new todoItem_1.TodoItem(2, "Learn new thing"),
@@ -9,9 +10,28 @@ let thingsTodo = [
 ];
 // create a variable that add thingsToDo in TodoCollection class
 let collection = new todoCollection_1.TodoCollection("Awon", thingsTodo);
-console.clear();
-// display the number of incomplete items, along with completed items in console
-console.log(`${collection.userName}'s Todo List`
-    + `(${collection.getItemCounts().incomplete} items to do)`);
-// call TodoItems method from TodoCollection
-collection.getTodoItems(true).forEach(item => item.printDetails());
+// add statements to make us of inquirer
+function displayTodoList() {
+    console.log(`${collection.userName}'s Todo List`
+        + `(${collection.getItemCounts().incomplete} items to do)`);
+    collection.getTodoItems(true).forEach(item => item.printDetails());
+}
+var Commands;
+(function (Commands) {
+    Commands["Quit"] = "Quit";
+})(Commands || (Commands = {}));
+function promptUser() {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: 'list',
+        name: 'command',
+        message: 'Choose option',
+        choices: Object.values(Commands)
+    }).then(answers => {
+        if (answers['command'] !== Commands.Quit) {
+            promptUser();
+        }
+    });
+}
+promptUser();
--- assignment1/muhammad-awon/todo/src/index.ts
+++ assignment1/muhammad-awon/todo/src/index.ts
@@ -1,5 +1,6 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
+import * as inquirer from 'inquirer';
 
 // create a variable to store list of ids and tasks
 let thingsTodo: TodoItem[] = [
@@ -10,10 +11,30 @@ let thingsTodo: TodoItem[] = [
 // create a variable that add thingsToDo in TodoCollection class
 let collection: TodoCollection = new TodoCollection("Awon", thingsTodo);
 
-console.clear();
-// display the number of incomplete items, along with completed items in console
-console.log(`${collection.userName}'s Todo List`
-+ `(${collection.getItemCounts().incomplete} items to do)`);
+// add statements to make us of inquirer
+function displayTodoList(): void {
+    console.log(`${collection.userName}'s Todo List`
+    + `(${collection.getItemCounts().incomplete} items to do)`);
+    collection.getTodoItems(true).forEach(item=>item.printDetails());
+}
 
-// call TodoItems method from TodoCollection
-collection.getTodoItems(true).forEach(item=>item.printDetails());
+enum Commands {
+    Quit = 'Quit'
+}
+
+function promptUser(): void {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: 'list',
+        name: 'command',
+        message: 'Choose option',
+        choices: Object.values(Commands)
+    }).then(answers=>{
+        if (answers['command'] !== Commands.Quit) {
+            promptUser();
+        }
+    })
+}
+
+promptUser();
