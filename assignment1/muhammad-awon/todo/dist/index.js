4defd2d9288b1bcb0aa9c21c54107f2f78f5ca30
Muhammad Awon <awon195@gmail.com>
2021-07-14 16:49:38 +0500

    add Complete Task & Remove Task option

--- assignment1/muhammad-awon/todo/dist/index.js
+++ assignment1/muhammad-awon/todo/dist/index.js
@@ -21,7 +21,9 @@ function displayTodoList() {
 var Commands;
 (function (Commands) {
     Commands["Add"] = "Add New Task";
+    Commands["Complete"] = "Complete Task";
     Commands["Toggle"] = "Show/Hide Completed";
+    Commands["Purge"] = "Remove Completed Tasks";
     Commands["Quit"] = "Quit";
 })(Commands || (Commands = {}));
 // create function to allow user to add new tasks
@@ -32,6 +34,18 @@ function promptAdd() {
         if (answers['add'] !== "") {
             collection.addTodo(answers['add']);
         }
+        promptUser();
+    });
+}
+// create function to allow user to mark completed tasks
+function promptComplete() {
+    console.clear();
+    inquirer.prompt({ type: 'checkbox', name: 'complete',
+        message: 'Mark Tasks Complete',
+        choices: collection.getTodoItems(showCompleted).map(item => ({ name: item.task, value: item.id, checked: item.complete })) })
+        .then(answers => {
+        let completedTasks = answers['complete'];
+        collection.getTodoItems(true).forEach(item => collection.markComplete(item.id, completedTasks.find(id => id === item.id) != undefined));
         promptUser();
     });
 }
@@ -51,6 +65,18 @@ function promptUser() {
                 break;
             case Commands.Add:
                 promptAdd();
+                break;
+            case Commands.Complete:
+                if (collection.getItemCounts().incomplete > 0) {
+                    promptComplete();
+                }
+                else {
+                    promptUser();
+                }
+                break;
+            case Commands.Purge:
+                collection.removeComplete();
+                promptUser();
                 break;
         }
     });
--- assignment1/muhammad-awon/todo/src/index.ts
+++ assignment1/muhammad-awon/todo/src/index.ts
@@ -23,7 +23,9 @@ function displayTodoList(): void {
 
 enum Commands {
     Add = 'Add New Task',
+    Complete = 'Complete Task',
     Toggle = 'Show/Hide Completed',
+    Purge = 'Remove Completed Tasks',
     Quit = 'Quit'
 }
 
@@ -36,6 +38,22 @@ function promptAdd(): void {
         }
         promptUser();
     })
+}
+
+// create function to allow user to mark completed tasks
+function promptComplete(): void {
+    console.clear();
+    inquirer.prompt({type: 'checkbox', name: 'complete',
+        message: 'Mark Tasks Complete',
+    choices: collection.getTodoItems(showCompleted).map(item=>
+        ({name: item.task, value: item.id, checked: item.complete}))})
+        .then(answers=>{
+            let completedTasks = answers['complete'] as number[];
+            collection.getTodoItems(true).forEach(item=>
+                collection.markComplete(item.id,
+                    completedTasks.find(id=> id === item.id) != undefined));
+            promptUser();
+        })
 }
 
 function promptUser(): void {
@@ -54,6 +72,17 @@ function promptUser(): void {
                 break;
             case Commands.Add:
                 promptAdd();
+                break;
+            case Commands.Complete:
+                if (collection.getItemCounts().incomplete > 0) {
+                    promptComplete();
+                } else {
+                    promptUser();
+                }
+                break;
+            case Commands.Purge:
+                collection.removeComplete();
+                promptUser();
                 break;
         }
     })
