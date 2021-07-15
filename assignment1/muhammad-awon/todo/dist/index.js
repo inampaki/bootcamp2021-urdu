8db4ea00ccda62cde66966c0f269b9a03cd4a632
Muhammad Awon <awon195@gmail.com>
2021-07-15 18:20:44 +0500

    modify index.js

--- assignment1/muhammad-awon/todo/dist/index.js
+++ assignment1/muhammad-awon/todo/dist/index.js
@@ -1,20 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const todoItem_1 = require("./todoItem");
-const todoCollection_1 = require("./todoCollection");
 const inquirer = require("inquirer");
+const jsonTodoCollection_1 = require("./jsonTodoCollection");
 // create a variable to store list of ids and tasks
 let thingsTodo = [
     new todoItem_1.TodoItem(1, "Morning workout"), new todoItem_1.TodoItem(2, "Learn new thing"),
     new todoItem_1.TodoItem(3, "Code time"), new todoItem_1.TodoItem(4, "Help someone", true)
 ];
-// create a variable that add thingsToDo in TodoCollection class
-let collection = new todoCollection_1.TodoCollection("Awon", thingsTodo);
+// create a variable that add thingsToDo in JsonTodoCollection class
+let collection = new jsonTodoCollection_1.JsonTodoCollection("Awon", thingsTodo);
 // create a variable to show completed tasks
 let showCompleted = true;
-// add statements to make use of inquirer
 function displayTodoList() {
-    console.log(`${collection.userName}'s Todo List`
+    console.log(`${collection.userName}'s Todo List `
         + `(${collection.getItemCounts().incomplete} items to do)`);
     collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
 }
@@ -26,25 +25,23 @@ var Commands;
     Commands["Purge"] = "Remove Completed Tasks";
     Commands["Quit"] = "Quit";
 })(Commands || (Commands = {}));
-// create function to allow user to add new tasks
 function promptAdd() {
     console.clear();
-    inquirer.prompt({ type: 'input', name: 'add', message: 'Enter Task:' })
+    inquirer.prompt({ type: "input", name: "add", message: "Enter task:" })
         .then(answers => {
-        if (answers['add'] !== "") {
-            collection.addTodo(answers['add']);
+        if (answers["add"] !== "") {
+            collection.addTodo(answers["add"]);
         }
         promptUser();
     });
 }
-// create function to allow user to mark completed tasks
 function promptComplete() {
     console.clear();
-    inquirer.prompt({ type: 'checkbox', name: 'complete',
-        message: 'Mark Tasks Complete',
-        choices: collection.getTodoItems(showCompleted).map(item => ({ name: item.task, value: item.id, checked: item.complete })) })
-        .then(answers => {
-        let completedTasks = answers['complete'];
+    inquirer.prompt({ type: "checkbox", name: "complete",
+        message: "Mark Tasks Complete",
+        choices: collection.getTodoItems(showCompleted).map(item => ({ name: item.task, value: item.id, checked: item.complete }))
+    }).then(answers => {
+        let completedTasks = answers["complete"];
         collection.getTodoItems(true).forEach(item => collection.markComplete(item.id, completedTasks.find(id => id === item.id) != undefined));
         promptUser();
     });
@@ -53,12 +50,12 @@ function promptUser() {
     console.clear();
     displayTodoList();
     inquirer.prompt({
-        type: 'list',
-        name: 'command',
-        message: 'Choose option',
-        choices: Object.values(Commands)
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands),
     }).then(answers => {
-        switch (answers['command']) {
+        switch (answers["command"]) {
             case Commands.Toggle:
                 showCompleted = !showCompleted;
                 promptUser();
