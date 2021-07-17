c9ce55da6a24c25362430db94a05057cc06ce137
hinamuslim8772 <69014775+hinamuslim8772@users.noreply.github.com>
2021-07-18 01:32:23 +0500

    2nd assignment

---
+++ class_5(v3)/src/index.ts
@@ -0,0 +1,11 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+let todos: TodoItem[] = [
+    new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
+    new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
+let collection: TodoCollection = new TodoCollection("Adam", todos);
+console.clear();
+console.log(`${collection.userName}'s Todo List`);
+let newId: number = collection.addTodo("Go for run");
+let todoItem: TodoItem = collection.getTodoById(newId);
+todoItem.printDetails();
