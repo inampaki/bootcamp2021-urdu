064ddb9b70212f9dd8ccec91bdf84667f3b60b8f
abeer0348 <abeerulhaq8@gmail.com>
2021-07-18 21:41:31 +0500

    v8 using a third party package inquirer

--- Assignment/Abeer/todo/src/index.ts
+++ Assignment/Abeer/todo/src/index.ts
@@ -1,5 +1,6 @@
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
+import * as inquirer from "inquirer";
 let todos: TodoItem[] = [
   new TodoItem(1, "Buy Flowers"),
   new TodoItem(2, "Get Shoes"),
@@ -7,9 +8,30 @@ let todos: TodoItem[] = [
   new TodoItem(4, "Call Joe", true),
 ];
 let collection: TodoCollection = new TodoCollection("Adam", todos);
-console.clear();
-console.log(
-  `${collection.userName}'s Todo List ` +
-    `(${collection.getItemCounts().incomplete} items to do)`
-);
-collection.getTodoItems(true).forEach((item) => item.printDetails());
+function displayTodoList(): void {
+  console.log(
+    `${collection.userName}'s Todo List ` +
+      `(${collection.getItemCounts().incomplete} items to do)`
+  );
+  collection.getTodoItems(true).forEach((item) => item.printDetails());
+}
+enum Commands {
+  Quit = "Quit",
+}
+function promptUser(): void {
+  console.clear();
+  displayTodoList();
+  inquirer
+    .prompt({
+      type: "list",
+      name: "command",
+      message: "Choose option",
+      choices: Object.values(Commands),
+    })
+    .then((answers) => {
+      if (answers["command"] !== Commands.Quit) {
+        promptUser();
+      }
+    });
+}
+promptUser();
