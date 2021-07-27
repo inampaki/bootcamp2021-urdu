358f7316eec2650de012883abdce692418f4abb0
UmarRafaqat <62068306+UmarRafaqat@users.noreply.github.com>
2021-07-27 11:47:27 +0500

    Assignment-1-Todoapp

---
+++ Assignment/version1/dist/index.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todoItem_1 = require("./todoItem");
+const todoCollection_1 = require("./todoCollection");
+const inquirer = require("inquirer");
+let todos = [
+    new todoItem_1.TodoItem(1, "Buy Flowers"), new todoItem_1.TodoItem(2, "Get Shoes"),
+    new todoItem_1.TodoItem(3, "Collect Tickets"), new todoItem_1.TodoItem(4, "Call Joe", true)
+];
+let collection = new todoCollection_1.TodoCollection("Adam", todos);
+function displayTodoList() {
+    console.log(`${collection.userName}'s Todo List `
+        + `(${collection.getItemCounts().incomplete} items to do)`);
+    collection.getTodoItems(true).forEach(item => item.printDetails());
+}
+var Commands;
+(function (Commands) {
+    Commands["Quit"] = "Quit";
+})(Commands || (Commands = {}));
+function promptUser() {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands)
+    }).then(answers => {
+        if (answers["command"] !== Commands.Quit) {
+            promptUser();
+        }
+    });
+}
+promptUser();
---
+++ Assignment/version2/dist/index.js
@@ -0,0 +1,54 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todoItem_1 = require("./todoItem");
+const todoCollection_1 = require("./todoCollection");
+const inquirer = require("inquirer");
+let todos = [
+    new todoItem_1.TodoItem(1, "Buy Flowers"), new todoItem_1.TodoItem(2, "Get Shoes"),
+    new todoItem_1.TodoItem(3, "Collect Tickets"), new todoItem_1.TodoItem(4, "Call Joe", true)
+];
+let collection = new todoCollection_1.TodoCollection("Adam", todos);
+let showCompleted = true;
+function displayTodoList() {
+    console.log(`${collection.userName}'s Todo List `
+        + `(${collection.getItemCounts().incomplete} items to do)`);
+    collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
+}
+var Commands;
+(function (Commands) {
+    Commands["Add"] = "Add New Task";
+    Commands["Toggle"] = "Show/Hide Completed";
+    Commands["Quit"] = "Quit";
+})(Commands || (Commands = {}));
+function promptAdd() {
+    console.clear();
+    inquirer.prompt({ type: "input", name: "add", message: "Enter task:" })
+        .then(answers => {
+        if (answers["add"] !== "") {
+            collection.addTodo(answers["add"]);
+        }
+        promptUser();
+    });
+}
+function promptUser() {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands),
+        //badProperty: true
+    }).then(answers => {
+        switch (answers["command"]) {
+            case Commands.Toggle:
+                showCompleted = !showCompleted;
+                promptUser();
+                break;
+            case Commands.Add:
+                promptAdd();
+                break;
+        }
+    });
+}
+promptUser();
